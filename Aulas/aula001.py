759bb54244c95f628c7ca83f7bee7c93182dfaac
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-03-06 16:48:37 -0300

    Update aula001.py

--- Aulas/aula001.py
+++ Aulas/aula001.py
@@ -1,4 +1,11 @@
 n1 = int(input('Digite um número: '))
-print (type(n1))
+n2 = int(input('Digite outro número: '))
+print(type(n1))
+print(type(n2))
+
+s = n1 + n2
+print(f'A soma dos números digitados é {s}')
+
 #Aqui no código a cima, usando o type, conseguimos identificar a classe, nesse caso um inteiro, após a conversão
+
 
