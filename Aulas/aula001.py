af1a9242c4ff85de71edada04d0c089a54575383
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-03-06 16:44:38 -0300

    Update aula001.py

--- Aulas/aula001.py
+++ Aulas/aula001.py
@@ -1,4 +1,4 @@
-n1 = input('Digite um número: ')
+n1 = int(input('Digite um número: '))
 print (type(n1))
-#Aqui no código a cima, usando o type, conseguimos identificar a classe, nesse caso uma String
+#Aqui no código a cima, usando o type, conseguimos identificar a classe, nesse caso um inteiro, após a conversão
 
