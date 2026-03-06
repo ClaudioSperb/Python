3f16d065fc645129365b9d518748b0eae50068bc
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-03-06 10:38:21 -0300

    Criamos nosso primeiro scrip em Python

---
+++ Scripts-python/script-teste-2.py
@@ -0,0 +1,8 @@
+nome = input('Digite seu nome -> ')
+print ('Seja bem vindo ' + nome)
+idade = input('Agora me diga sua idade ->')
+print ('Certo',nome + ', você tem' ,idade + ' anos de idade.')
+anoNascimento = input ('Agora me diga o ano do seu nascimento ->')
+print ('O ano que você digitou é' , anoNascimento,'.')
+print ('Obrigado pelas respostas',nome + ', tenha um ótimo dia')
+
