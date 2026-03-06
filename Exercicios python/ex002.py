c390bc24f11c406f9e4e752efd2295cb246ce692
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-03-06 14:10:23 -0300

    Update ex002.py

--- Exercicios python/ex002.py
+++ Exercicios python/ex002.py
@@ -1,4 +1,3 @@
 nome = input('Digite seu nome _> ')
-print('Seja muito bem vindo {} é uma prazer te conhecer!'.format(nome))
-
-
+#print('Seja muito bem vindo {} é uma prazer te conhecer!'.format(nome))
+print(f'Seja bem vindo {nome}, é uma prazer te conhecer!')
