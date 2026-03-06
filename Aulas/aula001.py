9d3176b7c9444c84a10c339b1a744867d720e8bb
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-03-06 17:01:39 -0300

    Update aula001.py

--- Aulas/aula001.py
+++ Aulas/aula001.py
@@ -8,7 +8,6 @@ m = n1 * n2
 print(f'A soma dos números digitados é {s}')
 print(f'A multiplicação dos numeros é {m}')
 
-
 #Aqui no código a cima, usando o type, conseguimos identificar a classe, nesse caso um inteiro, após a conversão
 
 
