49bf290f953a7e102720585a2f0db2af5ea6513c
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-03-06 11:56:55 -0300

    Create script-teste-5.py

---
+++ Scripts-python/script-teste-5.py
@@ -0,0 +1,8 @@
+num1 = float(input('Numero 1 -> '))
+num2 = float(input('Numero 2 -> '))
+soma = num1 + num2
+print ('A soma dos numeros é',soma)
+mult = num1 * num2
+print ('A mltiplicação entre os numeros é', mult)
+exp = soma ** 2
+print ('O resultado ao quadrado da sua soma é', exp)
