a04444368ebd9e6d2144a6a263492fc9e88a3a5c
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-03-06 10:45:24 -0300

    Create script-teste-3.py

---
+++ Scripts-python/script-teste-3.py
@@ -0,0 +1,4 @@
+dia = input('Digite o dia do seu nascimento ->')
+mes = input('Agora digite o mes do seu nascimento ->')
+ano = input('E pra finalizar, digite o ano do seu nascimento ->')
+print ('você nasceu dia',dia,'de', mes,'de', ano)
