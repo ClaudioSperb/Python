d3e26e3c889e892c9e7194f70be46f8884e18aa4
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-03-09 14:11:42 -0300

    Update Aula007.py

--- Aulas/Aula007.py
+++ Aulas/Aula007.py
@@ -1,3 +1,13 @@
 n1 = int(input('Digite um numero: '))
 n2 = int(input('Digite outro numero: '))
+soma = n1 + n2
+subt = n1 - n2
+mult = n1 * n2
+divnor = n1 / n2
+divint = n1 // n2
+pot = n1**n2
+restdiv = n1 % n2
+
 print(f'O valor da soma entre eles é {n1+n2}')
+print(f'O valor da subtração é {n1 - n2}, da multiplicação é {mult}, da divisão é {divnor:.3F}')
+print(f'O valor da divisão inteira é {divint}, da potencia é {pot} e do resto da divisão é {restdiv}')
