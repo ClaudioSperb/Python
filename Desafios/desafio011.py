e3272f7dd1e68a330986df593d08644ef8e446bd
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-03-11 15:02:17 -0300

    Update desafio011.py

--- Desafios/desafio011.py
+++ Desafios/desafio011.py
@@ -2,5 +2,7 @@ larg = float (input('Digite a largura da sua parede: '))
 alt = float (input('Agora, me passe a altura da parede: '))
 areaTotal = larg * alt
 lataTinta = (areaTotal * 1) / 2
+galaoTinta = (lataTinta / 18)
 print(f'As medidas da sua parede é {larg} x {alt}, a area total é de {areaTotal} m² quadrados')
 print(f'Você precisa comprar {lataTinta:.2f}l de tinta')
+print(f'Você vai precisar de {galaoTinta:.2f} galoes de 18l')
