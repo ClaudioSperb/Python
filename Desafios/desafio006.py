e5b42128ea850ec19f5e1d60a3a3546ebc93b04b
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-03-11 12:14:27 -0300

    Update desafio006.py

--- Desafios/desafio006.py
+++ Desafios/desafio006.py
@@ -1,2 +1,4 @@
 num = int(input('Digite um numero: '))
 print(f' O numero digitado é o {num},\n o dobro desse numero é {num * 2},\n o triplo é {num * 3}\n e a raiz quadrada é {num ** 0.5}')
+#Para fazer raiz quadrada pode ser usado de outras forma tambem, exemplo a baixo
+print(f' A raiz quadrada de {num} é {pow(num,(1/2))}')
