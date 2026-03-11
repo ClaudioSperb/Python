3733e3a826d854d9da36213e7cd34abb8b33d08e
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-03-11 16:49:08 -0300

    atualização

--- Desafios/desafio012.py
+++ Desafios/desafio012.py
@@ -1,3 +1,3 @@
-valorItem = float (input('Digite o valor de item desejado: '))
+valorItem = float (input('Digite o valor de item desejado: R$'))
 desconto = valorItem * (5 / 100)
 print(f'O item tem o valor de R${valorItem:.2f}, com 5% de desconto, seu item fica R${valorItem - desconto:.2f} reais')
--- Desafios/desafio013.py
+++ Desafios/desafio013.py
@@ -1,3 +1,7 @@
-valorSalario = float(input('Digite o valor do seu salario: '))
+valorSalario = float(input('Digite o valor do seu salario: R$'))
 aumentoSalario = valorSalario * (15 / 100)
 print(f'Atualmente seu salário é de R${valorSalario:.2f} \ncom o aumento de 15% seu salário passou para \nR${valorSalario + aumentoSalario:.2f} Reais')
+print('-' * 25)
+
+dicidio2025 = valorSalario + (valorSalario * 7.5 / 100)
+print(f'O valor reajustado com o Dicidio de 7.5% fica {dicidio2025:.2f} reais')
