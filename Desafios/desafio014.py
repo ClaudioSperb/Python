488cd0e15b1824b59807dd2e23848c44af65b1f3
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-03-12 08:32:59 -0300

    Update desafio014.py

--- Desafios/desafio014.py
+++ Desafios/desafio014.py
@@ -2,7 +2,7 @@ grauCelsios = float(input('Digite a temperatura de sua Cidade: ºC >>>'))
 fahrenheit = (grauCelsios * 1.8) + 32
 kelvin = grauCelsios + 273.15
 print('-' * 15)
-print(f'A sua cidade esta com {grauCelsios}ºC, convertendo para Fahrenheit ela fica {fahrenheit}ºF')
+print(f'A sua cidade esta com {grauCelsios}ºC, convertendo para Fahrenheit fica {fahrenheit}ºF')
 print('-' * 15)
 print(f'Em Kelvin sua temperatura é de {kelvin}K')
 
