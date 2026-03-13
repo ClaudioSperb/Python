52744576651b8ba83ca5a4234f24ec7712d03720
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-03-13 17:21:10 -0300

    Update desafio019.py

--- Desafios/desafio019.py
+++ Desafios/desafio019.py
@@ -1,4 +1,4 @@
 import random
 engateToro = input('Aperte o [ENTER] e veja quem vai instalar o engate da toro 2017: ')
-listaAlunos = random.choice(['Claudio', 'Elton', 'Nildo', 'Cristen', 'Vitor', 'Fernando', 'Dyeiki'])
+listaAlunos = random.choice(['Claudio', 'Elton', 'Nildo', 'Cristen', 'Vitor', 'Fernando', 'Dyeiki', 'Felipe'])
 print(f'O sortudo da vez foi o {listaAlunos}')
