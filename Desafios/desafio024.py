58cc50731b92d8af70c2ffcaa22415b2e3f76eed
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-03-18 16:06:54 -0300

    desafio criado

---
+++ Desafios/desafio024.py
@@ -0,0 +1,6 @@
+nomeCidade = str(input('Digite o nome da sua cidade: ')).upper()
+validandoCidade ='SANTO' in nomeCidade
+print(f'O nome da cidade é {nomeCidade}')
+print('Validando se a cidade possui "SANTO" em seu nome ....  ')
+print(f'Validação concluida >>>> {validandoCidade}')
+
---
+++ Desafios/desafio025.py
@@ -0,0 +1,5 @@
+nomeCompleto = str(input('Digite seu nome completo: ')).strip().upper()
+validandoNome = 'SILVA' in nomeCompleto
+print(f'Seu nome completo é {nomeCompleto} >>>> ')
+print(f'Validando se o nome possui "SILVA" >>> ......')
+print(f'Validação concluída: {validandoNome}')
