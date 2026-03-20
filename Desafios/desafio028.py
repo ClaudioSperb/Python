67d0033ccf2bcc7b1fe3b0b6f55acb7fc79b3fd5
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-03-20 16:33:27 -0300

    Update desafio028.py

--- Desafios/desafio028.py
+++ Desafios/desafio028.py
@@ -1,7 +1,11 @@
 import random
+from time import sleep
 num = int(input('Digite um numero de 0 a 5: '))
+print('PROCESSANDO')
+sleep(3)
 numMaquina = random.randint(0, 5)
 print(f'O numero escolhido pela Máquina foi >>> {numMaquina}')
+sleep(2)
 if num == numMaquina:
     print('Muito bem !!!, você advinhou o número')
 else:
