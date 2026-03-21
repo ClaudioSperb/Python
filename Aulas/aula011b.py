6ce1a641259b4b5e4aa4f038ff72d08ee6e9360b
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-03-21 11:48:05 -0300

    Update aula011b.py

--- Aulas/aula011b.py
+++ Aulas/aula011b.py
@@ -3,7 +3,7 @@ multa = (vel - 80) * 7
 print(f'Sua velocidade é de {vel:.1f}Km/h')
 print('RADAR - Analisando sua Velocidade...')
 if vel > 80:
-    print(f'Você esta a {vel}Km/h e foi \033[1;31mMULTADO !!!')
-    print(f'\033[1;97;43mO valor da multa é de R${multa:.2f} reais.')
+    print(f'Você esta a {vel}Km/h e foi \033[1;97;41m  MULTADO !!!\033[m')
+    print(f'\033[1;97;43mO valor da multa é de R${multa:.2f} reais.\033[m')
 else:
     print(f'\033[1;32mVocê esta a {vel}Km/h - BOA VIAGEM !!!!')
