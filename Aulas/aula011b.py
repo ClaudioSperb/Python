07fca3d12f1c050c3fa7b4d0ef0474fd50a159bc
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-03-21 11:49:28 -0300

    Update aula011b.py

--- Aulas/aula011b.py
+++ Aulas/aula011b.py
@@ -1,7 +1,10 @@
+from time import sleep
+
 vel = float(input('Digite sua velocidade atual: '))
 multa = (vel - 80) * 7
 print(f'Sua velocidade é de {vel:.1f}Km/h')
 print('RADAR - Analisando sua Velocidade...')
+sleep(3)
 if vel > 80:
     print(f'Você esta a {vel}Km/h e foi \033[1;97;41m  MULTADO !!!\033[m')
     print(f'\033[1;97;43mO valor da multa é de R${multa:.2f} reais.\033[m')
