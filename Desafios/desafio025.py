dbc39c73b2f78f4dbcfe856880317400752e9add
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-03-23 11:08:00 -0300

    Update desafio025.py

--- Desafios/desafio025.py
+++ Desafios/desafio025.py
@@ -1,5 +1,9 @@
+from time import sleep
+
 nomeCompleto = str(input('Digite seu nome completo: ')).strip().upper()
 validandoNome = 'SILVA' in nomeCompleto
 print(f'Seu nome completo é {nomeCompleto} >>>> ')
 print(f'Validando se o nome possui "SILVA" >>> ......')
+sleep(2)
+print('.........')
 print(f'Validação concluída: {validandoNome}')
